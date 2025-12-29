560aa8c6dfaf1022d81a0a0978edc1bc45148f02
hidden hero <hiddenhero47pro@gmail.com>
2025-12-29 22:28:53 +0100

    changes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,6 +3,7 @@ import {
 	Container,
 	IntroSection,
 	ContainerSection,
+	AppFooter,
 } from './elements/index.style';
 import { useOutletContext } from 'react-router-dom';
 import postImage from '../../assets/images/image.avif';
@@ -12,9 +13,21 @@ import { Link } from 'react-router-dom';
 import { IoIosArrowForward } from 'react-icons/io';
 import { items } from '../../dummyData/shopItems';
 import ShopItem from '../../components/shop-item-display-unit/index';
+import {
+	FaInstagram,
+	FaXTwitter,
+	FaFacebookF,
+	FaTiktok,
+} from 'react-icons/fa6';
+import { useTheme } from 'styled-components';
+import {
+	FooterBgD,
+	FooterBgL,
+} from '../../components/icon-components/backgrounds';
 
 function Index() {
 	const { aftermath } = useOutletContext();
+	const theme = useTheme();
 	return (
 		<Container className="Y_scroll_style">
 			<div id="myVideoPlayer">
@@ -70,7 +83,87 @@ function Index() {
 				/>
 			</ContainerSection>
 
-			<footer></footer>
+			<AppFooter>
+				<div id="footer_background">
+					{theme.mode === 'dark' ? (
+						<FooterBgD width="100%" height="100%" />
+					) : (
+						<FooterBgL width="100%" height="100%" />
+					)}
+				</div>
+				<div id="inner_wrapper">
+					<div id="list_info">
+						<Link to="/about-us" className="font-sans">
+							About
+						</Link>
+
+						<a
+							href="https://substack.com"
+							target="_blank"
+							rel="noreferrer"
+							className="font-sans"
+						>
+							Blog
+						</a>
+
+						<Link to="/about-us" className="font-sans">
+							Terms and Condition
+						</Link>
+
+						<Link to="/about-us" className="font-sans">
+							Privacy Policy
+						</Link>
+
+						<a
+							href="mailto:support@housemaduekwe.com"
+							target="_blank"
+							rel="noreferrer"
+							className="font-sans"
+						>
+							Email Us
+						</a>
+					</div>
+
+					<div className="flex justify-center items-center gap-[20px] text-[22px] mx-[auto]">
+						<a
+							href="https://instagram.com"
+							target="_blank"
+							rel="noreferrer"
+							className="transition-all duration-300 ease-in-out hover:text-[var(--intro-logo)] hover:scale-125"
+						>
+							<FaInstagram />
+						</a>
+						<a
+							href="https://twitter.com"
+							target="_blank"
+							rel="noreferrer"
+							className="transition-all duration-300 ease-in-out hover:text-[var(--intro-logo)] hover:scale-125"
+						>
+							<FaXTwitter />
+						</a>
+						<a
+							href="https://facebook.com"
+							target="_blank"
+							rel="noreferrer"
+							className="transition-all duration-300 ease-in-out hover:text-[var(--intro-logo)] hover:scale-125"
+						>
+							<FaFacebookF />
+						</a>
+						<a
+							href="https://tiktok.com"
+							target="_blank"
+							rel="noreferrer"
+							className="transition-all duration-300 ease-in-out hover:text-[var(--intro-logo)] hover:scale-125"
+						>
+							<FaTiktok />
+						</a>
+					</div>
+
+					<p className="font-sans">
+						© 2025 House Maduekwe, Inc. All rights reserved.
+					</p>
+				</div>
+			</AppFooter>
 		</Container>
 	);
 }
