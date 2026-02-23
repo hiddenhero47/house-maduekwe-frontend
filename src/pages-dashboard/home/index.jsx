3635ad82a0afd8ea90ce777fe325b1ae996853a8
hidden hero <hiddenhero47pro@gmail.com>
2026-02-23 15:00:45 +0100

    changes

--- src/pages-dashboard/home/index.jsx
+++ src/pages-dashboard/home/index.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from './elements/index.style';
 import CustomTable from '../../components/table_components/basicTableOne';
 import { NoDataIcon } from '../../components/icon-components/empty';
 import ImageSelector from '../../components/form-components/image-selector/selector';
 import { items } from '../../dummyData/shopItems';
+import ChipsInput from '../../components/form-components/chips-input/chips-input';
 
 function Index() {
+	const [tag, setTags] = useState([]);
 	const data = [
 		{
-			firstName: "Okonkwo",
-			lastName: "Charles",
-			email: "okonkwo@gmail.com",
+			firstName: 'Okonkwo',
+			lastName: 'Charles',
+			email: 'okonkwo@gmail.com',
 		},
 
 		{
-			firstName: "Okonkwo",
-			lastName: "Victor",
-			email: "victor@gmail.com",
+			firstName: 'Okonkwo',
+			lastName: 'Victor',
+			email: 'victor@gmail.com',
 		},
 
 		{
-			firstName: "Maduekwe",
-			lastName: "Chinadu",
-			email: "maduekwe@gmail.com",
+			firstName: 'Maduekwe',
+			lastName: 'Chinadu',
+			email: 'maduekwe@gmail.com',
 		},
 	];
 
@@ -38,18 +40,14 @@ function Index() {
 							return 'First Name';
 						},
 						accessor: 'firstName',
-						Cell: ({ value }) => (
-							<span className="nowrap">{value}</span>
-						),
+						Cell: ({ value }) => <span className="nowrap">{value}</span>,
 					},
 					{
 						Header: () => {
 							return 'Last Name';
 						},
 						accessor: 'lastName',
-						Cell: ({ value }) => (
-							<span className="nowrap">{value}</span>
-						),
+						Cell: ({ value }) => <span className="nowrap">{value}</span>,
 					},
 					{
 						Header: () => {
@@ -61,16 +59,37 @@ function Index() {
 				]}
 				dataSource={data || []}
 				emptyIcon={
-					<NoDataIcon width="150px" height="150px" color="var(--mainBody-sbText)" />
+					<NoDataIcon
+						width="150px"
+						height="150px"
+						color="var(--mainBody-sbText)"
+					/>
 				}
 				emptyText="NO ITEMS YET"
 				emptySbText="There is no available data to show. Please try something else"
-				refetch={()=> {}}
+				refetch={() => {}}
 				isLoading={false}
 				useStrip
 			/>
 
-			<ImageSelector component={<span>open</span>} options={ImageCatalog}/>
+			<ImageSelector component={<span>open</span>} options={ImageCatalog} />
+
+			<div className='w-[350px] flex flex-col mt-[50px]'>
+				<ChipsInput
+				id="tag"
+				name="tag"
+				value={tag}
+				onChange={(v) => setTags(v)}
+				max={10}
+				// onBlur={handleBlur}
+				isError={true}
+				errormessage="error"
+				placeholder="Add tag and press Enter"
+				paddingX="14px"
+				paddingY="9px"
+				useBackground
+			/>
+			</div>
 		</Container>
 	);
 }
